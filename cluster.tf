e0836a0669b6b7d27df9071cbb1638adc512f288
Matthew Barnes <winlinuxmatt@gmail.com>
2025-04-06 00:15:47 -0600

    fix

--- cluster.tf
+++ cluster.tf
@@ -172,7 +172,7 @@ resource "null_resource" "run_custom_script" {
     command = "terraform output -raw kubeconfig > ~/.kube/config && terraform output -raw talosconfig > ~/.talos/config && chmod 600 ~/.kube/config && chmod 600 ~/.talos/config"
   }
   depends_on = [
-    data.talos_cluster_kubeconfig.kubeconfig,
+    talos_cluster_kubeconfig.kubeconfig,
     data.talos_client_configuration.talosconfig,
     resource_name_for_health_check
   ]

e60ed8a0ebb803d0b49d86ecb63853801534da29
zombiehoffa <github@quilley.org>
2025-05-17 18:49:49 +0000

    Update main.tf
    added fluxcd stuff

--- main.tf
+++ main.tf
@@ -16,3 +16,29 @@ provider "proxmox" {
   password = var.proxmox_password # Use a secure method to manage sensitive data
   insecure = true                 # Only needed if your Proxmox server is using a self-signed certificate
 }
+
+#trying to get fluxcd working. You probably want to generate your ssh key for forgejo internally and then export the pub key to add to your forgejo, I had infra already setup so generated the key externally and just pointed it to it below.
+provider "flux" {
+  kubernetes = {
+#    host                   = [var.talos_cp_01_ip_addr]
+#    client_certificate     = taloscluster.taloscluster.client_certificate
+#    client_key             = taloscluster.taloscluster.client_key
+#    cluster_ca_certificate = taloscluster.talosccluster.cluster_ca_certificate
+host                   = talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.host
+client_certificate     = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.client_certificate)
+client_key             = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.client_key)
+cluster_ca_certificate = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.ca_certificate)
+  }
+  git = {
+    url = "ssh://git@${var.forgejo_host}:${var.forgejo_port}/${var.forgejo_org}/${var.forgejo_repository}.git"
+    ssh = {
+      username    = "git"
+      private_key = file("~/.ssh/id_ed25519")
+    }
+  }
+}
+#uncomment this and figure it out if you want to generate the forgejo instance internally and deploy to your cluster.
+#provider "forgejo" {
+#  host      = "http://${var.forgejo_host}:3000"
+#  api_token = var.forgejo_token
+#}
